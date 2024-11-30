753babf4a09cd899bd2a6008cc19f98338de0c4f
Javier Miranda <javsalcantar17@gmail.com>
2024-11-30 16:53:35 -0600

    Add files via upload

---
+++ MiNombre.java
@@ -0,0 +1,15 @@
+
+package ejer.solo;
+
+import java.util.Scanner;
+
+public class MiNombre {
+    public static void main(String Args[]){
+  Scanner sc= new Scanner(System.in);
+  System.out.println("Ingresa tu nombre por favor");
+  String nombre=sc.nextLine();
+        
+        System.out.println("Hola"+nombre+ "Bienvenido a tu proyecto");
+    }
+    
+}
